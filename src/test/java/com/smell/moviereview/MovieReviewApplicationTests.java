0a946daf190683412b7daeea2cfaddb5cd0c38a4
smellhyang <dlgiddk10@gmail.com>
2022-05-11 00:41:21 +0900

    issue5 sync

--- src/test/java/com/smell/moviereview/MovieReviewApplicationTests.java
+++ src/test/java/com/smell/moviereview/MovieReviewApplicationTests.java
@@ -1,13 +1,33 @@
 package com.smell.moviereview;
 
+import com.smell.moviereview.dao.ReviewDAO;
+import com.smell.moviereview.service.impl.ReviewService;
+import com.smell.moviereview.service.impl.ReviewerService;
+import com.smell.moviereview.vo.ReviewVO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+
 
 @SpringBootTest
 class MovieReviewApplicationTests {
 
+	@Autowired
+	private ReviewerService reviewerService;
+
+	@Autowired
+	private ReviewService reviewService;
+
 	@Test
-	void contextLoads() {
+	public void reviewTest() {
+
+
+
+		ReviewVO review = new ReviewVO();
+		review.setRating(4);
+		//review.setDate();
 	}
 
 }
